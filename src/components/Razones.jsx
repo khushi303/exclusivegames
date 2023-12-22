ada6921a456fbd007f87bc79637b907136e72bac
khushi303 <maankhushi303@gmail.com>
2023-12-22 10:44:18 +0530

    change in razon img

--- src/components/Razones.jsx
+++ src/components/Razones.jsx
@@ -8,10 +8,10 @@ const Razones = () => {
         <div className='razons_padding razon_ellipse1 position-relative'>
             <Container className='position-relative z-2'>
                 <Row className='flex-lg-row flex-column-reverse'>
-                    <Col lg={6} data-aos="fade-right" className='mt-lg-0 mt-4 pt-lg-0 pt-2 d-flex justify-content-center justify-content-lg-start'>
-                        <img src={Razonesimg} alt="Razonesimg" className='w-100 br_16 me-3 mw_558' />
+                    <Col lg={6} data-aos="fade-right" className='mt-lg-0 mt-4 br_16 pt-lg-0 pt-2 d-flex justify-content-center justify-content-lg-start'>
+                        <img src={Razonesimg} alt="Razonesimg" className='w-100 br_16 object-fit-cover' />
                     </Col>
-                    <Col lg={6} className='d-lg-flex flex-column justify-content-between align-items-start'>
+                    <Col lg={6} className='d-lg-flex flex-column justify-content-between align-items-start ps-lg-4'>
                         <h3 data-aos="fade-down" className='mb-0 ff_anton fs_xl fw-normal lh_120 text-white mb-4'>Más razones para elegirnos</h3>
                         <div data-aos="fade-down" className='razon_box cur_pointer'>
                             <div className='razon_padding_box d-flex'>
